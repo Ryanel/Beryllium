79c976876bf94af80b9b83a9c820b2fa6e94e741
Corwin McKnight <lioneltabre@gmail.com>
2014-01-11 15:26:04 -0800

    Added colors

--- src/klog.cpp
+++ src/klog.cpp
@@ -1,12 +1,37 @@
 #include <log.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <low_textmode.h>
 void klog(int mode, const char *title, const char *fmt, ...)
 {
+	switch (mode)
+	{
+		case LOG_PANIC:
+			textmode_setcolor(0,0xC);
+			break;
+		case LOG_SEVERE:
+			textmode_setcolor(0,0xC);
+			break;
+		case LOG_ERROR:
+			textmode_setcolor(0,0xC);
+			break;
+		case LOG_WARN:
+			textmode_setcolor(0,0xC);
+			break;
+		case LOG_INFO:
+			textmode_setcolor(0,0xC);
+			break;
+		case LOG_DEBUG:
+			textmode_setcolor(0,0xC);
+			break;
+		default:
+			textmode_setcolor(0,15);
+	}
 	printf("[%s]:", title);
 	va_list args;
 	int rv;
 	va_start(args, fmt);
 	rv = vprintf(fmt, args);
 	va_end(args);
+	textmode_resetcolor();
 }
