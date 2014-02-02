27f670baeaee3410b199acef5e45ab6a1914a4e1
Corwin McKnight <lioneltabre@gmail.com>
2014-02-02 08:21:10 -0800

    Removed GCS implementation, changed version info to printf, as it is not part of a log

--- src/init.cpp
+++ src/init.cpp
@@ -8,8 +8,7 @@ extern "C" void kernel_init()
 	asm("cli");
 	terminal_init();
 	serial_init();
-	serial_print("All messages will be relayed from the terminal to serial output\n");
-	klog(LOG_INFO,"KERN","Beryllium %s v. %s (%s) \n",BERYLLIUM_RELEASE,BERYLLIUM_VERSION,BERYLLIUM_SOURCE);
+	printf("Beryllium %s v. %s (%s) \n",BERYLLIUM_RELEASE,BERYLLIUM_VERSION,BERYLLIUM_SOURCE);
 	init_x86();
 	klog(LOG_INFO,"KERN","Finished initialising...\n");
 	klog(LOG_INFO,"KERN","Nothing to do: halting\n");
