7ef6ad0de9859f182e10561d8cb908c77fe69657
Corwin McKnight <lioneltabre@gmail.com>
2014-02-28 17:55:15 -0800

    Added memmove

--- src/includes/string.h
+++ src/includes/string.h
@@ -5,4 +5,5 @@ int strcmp (const char *str1,const char *str2);
 //unsigned int strlen(unsigned char *str);
 void *memcpy(void *dest,const void *src,size_t n);
 void *memset(void *dest,int val,size_t n);
+void memmove(void *dest, const void *src, size_t n);
 #endif
